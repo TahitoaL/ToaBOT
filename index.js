3533b8ea14a9b755173d4f7244ee2b48ae7b12ba
TahitoaL <tahitoa.leygue@gmail.com>
2018-07-07 15:22:59 +0200

    Add bot on ready

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const {URL} = require('url')
 
 const bot = new Discord.Client()
 
+bot.on('ready', () => {
+    console.log('Bot ready')
+    bot.user.setActivity('rien du tout')
+})
+
 bot.login(process.env.TOKEN)
